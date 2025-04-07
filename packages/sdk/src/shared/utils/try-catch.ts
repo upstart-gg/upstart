d00079b44ada64bd22de61f22837ef91c190a96a
Matt <matthias@etienne.in>
2025-04-07 14:45:00 +0200

    Fix try catch (#296)

--- packages/sdk/src/shared/utils/try-catch.ts
+++ packages/sdk/src/shared/utils/try-catch.ts
@@ -1,12 +1,12 @@
-type Result<T, E = Error> = [E | null, T | null];
+type Result<T = unknown, E extends Error = Error> = [null, T] | [E, null];
 
 /**
  * Try catch wrapper for async functions
  */
-export async function tryCatch<T, E extends Error>(promise: Promise<T>): Promise<Result<T>> {
+export async function tryCatch<T, E extends Error = Error>(promise: Promise<T>): Promise<Result<T, E>> {
   try {
     return [null, await promise];
   } catch (e) {
-    return [e === null ? new Error(`Error in tryCatch`) : e, null] as [E, null];
+    return [e instanceof Error ? e : new Error(`Error in tryCatch: ${e}`), null] as Result<T, E>;
   }
 }
