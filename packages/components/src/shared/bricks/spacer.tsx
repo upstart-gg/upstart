a4ace414f01a0413f78b4f0139909902cf85ff77
Matt <matthias@etienne.in>
2025-07-01 16:55:04 +0200

    fix wrong className

--- packages/components/src/shared/bricks/spacer.tsx
+++ packages/components/src/shared/bricks/spacer.tsx
@@ -11,21 +11,11 @@ const Divider = forwardRef<HTMLDivElement, BrickProps<Manifest>>(({ brick, edita
     <div
       ref={ref}
       className={tx(
-        // {
-        //   "h-px": props.orientation === "horizontal",
-        //   "w-px": props.orientation === "vertical",
-        // },
-        "flex-grow flex-shrink-0 mobile:w-full",
-        // {
-        //   "min-w-20": props.orientation === "horizontal",
-        //   "min-h-30": props.orientation === "vertical",
-        // },
+        "flex-grow flex-shrink-0 @mobile:w-full",
         editable &&
           "hover:(striped-bg bg-gray-100) group-hover/section:(striped-bg bg-gray-100) transition-colors duration-[200ms] delay-[100ms]",
         selected && "striped-bg bg-gray-100",
-        // props.orientation === "horizontal" && (props.size ? `w-[${props.size}] flex-grow-0` : "w-full"),
-        // props.orientation === "vertical" && (props.size ? `h-[${props.size}]` : "h-full"),
-        Object.values(styles),
+        ...Object.values(styles),
       )}
     />
   );
