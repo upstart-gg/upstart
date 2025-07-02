5ed8aecf9f7d4805202411150b4a83653cada4b2
Benjamin Darnault <benjamindarnault@gmail.com>
2025-07-02 11:06:37 +0200

    add button to create new datarecord

--- packages/components/src/editor/components/json-form/fields/datarecord.tsx
+++ packages/components/src/editor/components/json-form/fields/datarecord.tsx
@@ -1,6 +1,7 @@
 import type { DatarecordSettings } from "@upstart.gg/sdk/shared/bricks/props/datarecord";
-import { Select } from "@upstart.gg/style-system/system";
+import { IconButton, Select } from "@upstart.gg/style-system/system";
 import type { FC } from "react";
+import { BsDatabaseAdd } from "react-icons/bs";
 import { useDraft, useEditorHelpers } from "~/editor/hooks/use-editor";
 import { fieldLabel } from "../form-class";
 import type { FieldProps } from "./types";
@@ -25,7 +26,7 @@ export const DatarecordField: FC<FieldProps<DatarecordSettings | undefined>> = (
 
   return (
     <div className="layout-field basis-full">
-      <div className="flex items-start flex-wrap gap-x-4 gap-y-1">
+      <div className="flex items-start text-center flex-wrap gap-x-4 gap-y-1">
         <div className="flex justify-between gap-1 flex-1">
           <label className={fieldLabel}>Database</label>
           <Select.Root defaultValue={currentValue} size="2" onValueChange={(value) => onChange(value)}>
@@ -50,12 +51,12 @@ export const DatarecordField: FC<FieldProps<DatarecordSettings | undefined>> = (
         <button
           type="button"
           onClick={() => {
-            // Handle create new action
-            // console.log("Create new datarecord action triggered");
             editorHelpers.onShowPopup?.("add-form-schema");
           }}
         >
-          create new
+          <IconButton variant="ghost" size="1" mr="2" radius="large" aria-label="Create new datarecord">
+            <BsDatabaseAdd size={20} />
+          </IconButton>
         </button>
       </div>
     </div>
