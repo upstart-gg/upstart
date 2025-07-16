e7c86246d13fb99e63a0da11498721148c0ffa25
Matt <matthias@etienne.in>
2025-07-16 11:40:10 +0200

    wip

--- packages/components/src/editor/components/json-form/HelpIcon.tsx
+++ packages/components/src/editor/components/json-form/HelpIcon.tsx
@@ -3,9 +3,9 @@ import { IoIosHelpCircleOutline } from "react-icons/io";
 
 export function HelpIcon({ help }: { help: string }) {
   return (
-    <Tooltip content={<span className="text-xs block p-1">{help}</span>} className="!z-[10000]" align="end">
-      <IconButton variant="ghost" size="2" radius="full" className="group !cursor-help" disabled>
-        <IoIosHelpCircleOutline className="text-upstart-400 group-hover:text-upstart-600" />
+    <Tooltip content={<span className="text-xs block p-1">{help}</span>} className="!z-[10000]">
+      <IconButton variant="ghost" size="3" radius="none" className="group !cursor-help !p-0 !mr-1">
+        <IoIosHelpCircleOutline className="text-upstart-400 group-hover:text-upstart-600 h-6 w-6" />
       </IconButton>
     </Tooltip>
   );
