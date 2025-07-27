3558be0da7e4ce7b05978b10b828ebd3a807fe74
Matt <matthias@etienne.in>
2025-07-27 16:47:09 +0200

    wip

--- packages/sdk/src/shared/bricks/manifests/box.manifest.ts
+++ packages/sdk/src/shared/bricks/manifests/box.manifest.ts
@@ -35,6 +35,7 @@ export const manifest = defineBrickManifest({
       title: "Direction",
       description: "Direction of the box layout",
     }),
+    // TODO: add also a justify content prop
     alignItems: Type.Optional(
       StringEnum(["items-start", "items-center", "items-end", "items-stretch"], {
         enumNames: ["Start", "Center", "End", "Stretch"],
