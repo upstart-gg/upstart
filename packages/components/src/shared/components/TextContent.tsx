b42955391750ff8551e10f9e8996316a77f957c2
Matt <matthias@etienne.in>
2025-07-31 22:05:47 +0200

    fix ref errors

--- packages/components/src/shared/components/TextContent.tsx
+++ packages/components/src/shared/components/TextContent.tsx
@@ -1,29 +1,24 @@
-import { forwardRef, lazy, Suspense } from "react";
+import { lazy, Suspense } from "react";
 import type { TextEditorProps } from "./TextEditor";
 
 const TextEditor = lazy(() => import("./TextEditor"));
 
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-const TextContent = forwardRef<HTMLDivElement, TextEditorProps<any>>(
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  (props: TextEditorProps<any>, ref) => {
-    if (props.editable) {
-      return (
-        <Suspense>
-          <TextEditor ref={ref} {...props} content={props.content} />
-        </Suspense>
-      );
-    }
-    const Component = props.as || "div";
+export default function TextContent(props: TextEditorProps<any>, ref: React.Ref<HTMLDivElement>) {
+  if (props.editable) {
     return (
-      <Component
-        ref={ref}
-        className={props.className}
-        // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
-        dangerouslySetInnerHTML={{ __html: props.content }}
-      />
+      <Suspense>
+        <TextEditor {...props} content={props.content} />
+      </Suspense>
     );
-  },
-);
-
-export default TextContent;
+  }
+  const Component = props.as || "div";
+  return (
+    <Component
+      ref={ref}
+      className={props.className}
+      // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
+      dangerouslySetInnerHTML={{ __html: props.content }}
+    />
+  );
+}
