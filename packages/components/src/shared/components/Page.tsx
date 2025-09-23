21c3cf537c7b15a7274efa43046f342ef9b9e952
Matt <matthias@etienne.in>
2025-09-23 10:34:30 +0200

    fix: restructure Page component to include title and meta tags

--- packages/components/src/shared/components/Page.tsx
+++ packages/components/src/shared/components/Page.tsx
@@ -1,25 +1,30 @@
 import { usePageStyle } from "~/shared/hooks/use-page-style";
 import Section from "./Section";
-import { usePageAttributes, useSections, useSiteAttributes } from "~/editor/hooks/use-page-data";
+import { usePage, usePageAttributes, useSections, useSiteAttributes } from "~/editor/hooks/use-page-data";
 
 export default function Page() {
   const pageClassName = usePageStyle();
   const sections = useSections();
+  const page = usePage();
   const siteAttributes = useSiteAttributes();
   const pageAttributes = usePageAttributes();
   return (
-    <div className={pageClassName}>
-      {sections.map((section) => (
-        <Section key={section.id} section={section} />
-      ))}
-      {siteAttributes.bodyTags && (
-        // biome-ignore lint/security/noDangerouslySetInnerHtml: Enable raw HTML
-        <div dangerouslySetInnerHTML={{ __html: siteAttributes.bodyTags }} />
-      )}
-      {pageAttributes.additionalTags?.bodyTags && (
-        // biome-ignore lint/security/noDangerouslySetInnerHtml: Enable raw HTML
-        <div dangerouslySetInnerHTML={{ __html: pageAttributes.additionalTags.bodyTags }} />
-      )}
-    </div>
+    <>
+      <title>{pageAttributes.title}</title>
+      <meta name="description" content={pageAttributes.description} />
+      <div className={pageClassName}>
+        {sections.map((section) => (
+          <Section key={section.id} section={section} />
+        ))}
+        {siteAttributes.bodyTags && (
+          // biome-ignore lint/security/noDangerouslySetInnerHtml: Enable raw HTML
+          <div dangerouslySetInnerHTML={{ __html: siteAttributes.bodyTags }} />
+        )}
+        {pageAttributes.additionalTags?.bodyTags && (
+          // biome-ignore lint/security/noDangerouslySetInnerHtml: Enable raw HTML
+          <div dangerouslySetInnerHTML={{ __html: pageAttributes.additionalTags.bodyTags }} />
+        )}
+      </div>
+    </>
   );
 }
