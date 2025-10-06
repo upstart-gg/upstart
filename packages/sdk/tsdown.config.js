8a6aac529ce37aabfec368bcda81abad43fa6a85
Benjamin Darnault <benjamindarnault@gmail.com>
2025-10-06 17:33:41 +0200

    undo

--- packages/sdk/tsdown.config.js
+++ packages/sdk/tsdown.config.js
@@ -24,7 +24,7 @@ export default defineConfig((options) => {
       outDir: "dist/shared",
       target: "esnext",
       nodeProtocol: true,
-      // unbundle: true,
+      unbundle: true,
       metafile: !!process.env.ANALYZE_BUNDLE,
       clean: !options.watch,
       sourcemap: options.watch ? "inline" : true,
