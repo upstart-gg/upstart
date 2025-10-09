474bab9441fe813a5e0cf17a0a2da149a30f2b2b
Benjamin Darnault <benjamindarnault@gmail.com>
2025-10-09 09:53:42 +0200

    fix import

--- packages/sdk/src/shared/attributes.ts
+++ packages/sdk/src/shared/attributes.ts
@@ -1,16 +1,16 @@
 import { Type, type Static } from "@sinclair/typebox";
+import { defaultsDeep } from "lodash-es";
 import { getSchemaDefaults } from "../shared/utils/schema";
-import { manifest as navbarManifest } from "./bricks/manifests/navbar.manifest";
 import { manifest as footerManifest } from "./bricks/manifests/footer.manifest";
+import { manifest as navbarManifest } from "./bricks/manifests/navbar.manifest";
+import { background } from "./bricks/props/background";
 import { boolean } from "./bricks/props/boolean";
-import { image } from "./bricks/props/image";
 import { colorPreset } from "./bricks/props/color-preset";
 import { queryUse, type QueryUseSettings } from "./bricks/props/dynamic";
+import { image } from "./bricks/props/image";
 import { querySchema } from "./datasources/types";
-import { StringEnum } from "./utils/string-enum";
-import { background } from "./bricks/props/background";
 import { toLLMSchema } from "./utils/llm";
-import defaultsDeep from "lodash-es/defaultsDeep";
+import { StringEnum } from "./utils/string-enum";
 
 // Default attributes
 export const pageAttributesSchema = Type.Object(
